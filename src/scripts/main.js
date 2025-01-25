f927cf82b38c7c427be334b0ffd6419135663a69
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-25 15:58:34 +0200

    sort list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function sortList(li) {
+  const items = [...li.querySelectorAll('li')];
+
+  items.sort((item1, item2) => {
+    const firstSalary = +item1.dataset.salary.slice(1).split(',').join('');
+
+    const secondSalary = +item2.dataset.salary.slice(1).split(',').join('');
+
+    return secondSalary - firstSalary;
+  });
+
+  items.forEach((item) => li.append(item));
+}
+
+sortList(list);
